270200c6a3585f4957ff91fe93213ddac7546344
John Breton <55368611+john-breton@users.noreply.github.com>
2022-03-08 08:11:22 -0500

    Include IntelliJ Code Inspect Suggestions

--- src/main/java/com/productreviews/services/UserServiceImpl.java
+++ src/main/java/com/productreviews/services/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
