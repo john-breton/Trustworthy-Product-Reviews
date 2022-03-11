473e48a38e377087f27c2c93ef04a89de5fc6cf6
sarahjaber <sarahjaber@cmail.carleton.ca>
2022-03-10 21:07:41 -0500

    Tests for Product Controller connection with product page and review page

---
+++ src/test/java/controllers/ProductControllerTest.java
@@ -0,0 +1,110 @@
+package controllers;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.productreviews.TrustworthyProductReviewsApplication;
+import com.productreviews.models.User;
+import com.productreviews.repositories.ProductRepository;
+import com.productreviews.repositories.UserRepository;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@AutoConfigureMockMvc(addFilters = false)
+@WithMockUser
+@SpringBootTest(classes= TrustworthyProductReviewsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    /**
+     * Test that a product page is rendered for a product and can use the product page
+     * to add reviews to the product
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser(username = "testuser")
+    public void ProductPageTest() throws Exception {
+
+        //user authentication
+        userRepository.save(new User("testuser", "testpassword"));
+        System.out.println(formLogin());
+
+        //add a product and check the connection
+        mockMvc.perform(get("/create/5")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(model().hasNoErrors())
+                .andExpect(status().is2xxSuccessful());
+
+        //add a product and check that the product is created
+        mockMvc.perform(get("/create/5")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(model().hasNoErrors())
+                .andExpect(content().string(containsString("Product Created")));
+
+        //access the product page and check that there initially no reviews
+        mockMvc.perform(get("/product/5")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(model().hasNoErrors())
+                .andExpect(content().string(containsString("No Reviews")));
+
+        //try to add a review to the product and check that the review is added on the review page
+        mockMvc.perform(get("/review/5?score=1&content=review1")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(model().hasNoErrors())
+                .andExpect(content().string(containsString("review1")));
+
+        //access the product page and check that the review is added
+        mockMvc.perform(get("/product/5")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(model().hasNoErrors())
+                .andExpect(content().string(containsString("review1")));
+
+    }
+
+}
