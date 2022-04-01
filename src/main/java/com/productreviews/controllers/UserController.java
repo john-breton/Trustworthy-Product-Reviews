02518b5e469ef80a832623b7752a357e386f7f5e
Mariam05 <tomariam@gmail.com>
2022-03-31 20:57:05 -0400

    tabs are persistent!!!

--- src/main/java/com/productreviews/controllers/UserController.java
+++ src/main/java/com/productreviews/controllers/UserController.java
@@ -1,12 +1,10 @@
 package com.productreviews.controllers;
 
-import com.productreviews.models.Product;
 import com.productreviews.models.User;
 import com.productreviews.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +12,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -75,18 +70,33 @@ public class UserController {
         return "user-page";
     }
 
-    @GetMapping("/follow/{username}")
-    public String follow(@PathVariable String username, Authentication authentication, Model model){
+    /**
+     * handles clicking on the 'follow' button in the user page.
+     * @param username
+     * @param redirectUsername
+     * @param authentication
+     * @param model
+     * @return
+     */
+    @GetMapping("/follow/{username}/{redirectUsername}")
+    public String follow(@PathVariable String username, @PathVariable(required = false) String redirectUsername,
+                         Authentication authentication, Model model){
         String currentUser = authentication.getName();
         User user = userRepository.findByUsername(currentUser);
+
         if (user == null || !authentication.isAuthenticated()) {
             log.error("User not found or not authenticated");
             return "error-page";
         }
+
+        if (redirectUsername == null){
+            redirectUsername = user.getUsername();
+        }
+
         User followedUser = userRepository.findByUsername(username);
         Objects.requireNonNull(user).addFollowing(followedUser);
         userRepository.save(user);
-        return "redirect:/user/" + followedUser.getUsername();
+        return "redirect:/user/" + redirectUsername;
     }
 
     /**
@@ -95,8 +105,8 @@ public class UserController {
      * @param model of the app
      * @return the page that confirms the unfollowing of the user
      */
-    @GetMapping("/unfollowUser/{username}/{redirect_username}")
-    public String unfollowUser(@PathVariable String username, @PathVariable(required = false) String redirect_username,
+    @GetMapping("/unfollowUser/{username}/{redirectUsername}")
+    public String unfollowUser(@PathVariable String username, @PathVariable(required = false) String redirectUsername,
                                Authentication authentication, Model model) {
 
 
@@ -109,15 +119,14 @@ public class UserController {
             return "error-page";
         }
 
-        User redirect_user = user;
-        if (redirect_username != null){
-            redirect_user = userRepository.findByUsername(redirect_username);
+        if (redirectUsername == null){
+            redirectUsername = unfollowed.getUsername();
         }
 
         user.unFollow(unfollowed);
         userRepository.save(user);
         model.addAttribute("user", unfollowed);
-        return "redirect:/user/" + redirect_user.getUsername();
+        return "redirect:/user/" + redirectUsername;
 
     }
 
